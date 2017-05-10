c0b2c07bee6047a90921a8a6d68b58abca38e620
thekatiemcneil <katie.mcneil@live.com>
2017-05-10 13:17:35 -0500

    Adding initial version of vampire detection program.

---
+++ ruby/vampires.rb
@@ -0,0 +1,24 @@
+#print "what is your name?"
+#assign user input to 'name' value.
+puts "Whats your name?"
+name = gets.chomp
+
+#print "how old are you?"
+#assign user input to 'age' value.
+puts "How old are you?"
+age = gets.chomp
+
+#print "What year were you born?"
+#assign user input to 'year of birth' value.
+puts "What year were you born?"
+year_of_birth = gets.chomp
+
+#print "our company cafeteria serves garlic bread. Should we order some for you?"
+#assign user input to 'garlic_bread_preference' value.
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic_bread_preference = gets.chomp
+
+#print "Would you like to enroll in the compnay's health insurance?"
+#assign user input to 'health_insurance' value.
+puts "Would you like to enroll in the company's health insurance?"
+health_insurance = gets.chomp
