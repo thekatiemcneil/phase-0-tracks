e5858a5bed25c4b8b19181c36cdb5358f0b89867
thekatiemcneil <katie.mcneil@live.com>
2017-05-10 17:06:10 -0500

    Adding initial vampire questions.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,2 +1,17 @@
+puts "What's your name?"
+  name = gets.chomp
 
+puts "How old are you?"
+  age = gets.chomp
+
+puts "What year were you born?"
+  year = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+  garic_bread = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+  insurance = gets.chomp
+
+puts "Thanks!"
 
