8376cc5137de20d7374d5f9ad5db6ba6792b2109
thekatiemcneil <katie.mcneil@live.com>
2017-05-10 17:30:42 -0500

    Removing vampire detector logic because it doesn't work.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,9 +1,9 @@
 puts "What's your name?"
   name = gets.chomp
 
-  if name = "Drake Cula"
+  if name == "Drake Cula"
     vampire_name = true
-  elsif name = "Tu Fang"
+  elsif name == "Tu Fang"
     vampire_name = true
   else vampire_name = false
   end
@@ -14,7 +14,7 @@ puts "How old are you?"
 puts "What year were you born?"
   year = gets.chomp
 
-2017 - year.to_i = estimated_age
+estimated_age = 2017 - year.to_i
 
   if estimated_age.to_i != age.to_i
     vampire_age = true
@@ -22,9 +22,9 @@ puts "What year were you born?"
   end
 
 puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-  garic_bread = gets.chomp
+  garlic_bread = gets.chomp
 
-  if garlic_bread = "no"
+  if garlic_bread == "no"
     vampire_garlic = true
   else vampire_garlic = false
   end
@@ -32,19 +32,10 @@ puts "Our company cafeteria serves garlic bread. Should we order some for you?"
 puts "Would you like to enroll in the company's health insurance?"
   insurance = gets.chomp
 
-  if insurance = "no"
+  if insurance == "no"
     vampire_health = true
   else vampire_health = false
   end
 
 puts "Thanks! Now detecting vampire status."
 
-if vampire_age && vampire_garlic == "false"
-  puts "Probably not a vampire."
-elsif vampire_age && (vampire_garlic || vampire_health) == "true"
-  puts "Probably a vampire."
-elsif vampire_age && vampire_garlic && vampire_health == "true"
-  puts "Almost certainly a vampire."
-elsif vampire_name == "true"
-  puts "Definitely a vampire."
-else puts "Results inconclusive."
