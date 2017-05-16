746d9d94c1f3df10c8234cc3f45bd1210d8d0dd5
thekatiemcneil <katie.mcneil@live.com>
2017-05-16 18:53:20 -0500

    Update with new methods.

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -7,33 +7,34 @@
 "iNvEsTiGaTiOn".swapcase
 => “InVeStIgAtIoN”
 
-# "zom".<???>
-# => “zoom”
+"zom".insert(1, 'o')
+=> “zoom”
 
-# "enhance".<???>
-# => "    enhance    "
+"enhance".center(20)
+=> "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
-# => "STOP! YOU’RE UNDER ARREST!"
+"Stop! You’re under arrest!".upcase
+=> "STOP! YOU’RE UNDER ARREST!"
 
 # "the usual".<???>
-#=> "the usual suspects"
+# => "the usual suspects"
 
 # " suspects".<???>
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
-# => "The case of the disappearing last lette"
+"The case of the disappearing last letter".chop
+=> "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
-# => "he mystery of the missing first letter"
+"The mystery of the missing first letter".chopfirst
+=> "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
-# => "Elementary, my dear Watson!"
+"Elementary,    my   dear        Watson!".squeeze
+=> "Elementary, my dear Watson!"
 
-# "z".<???>
-# => 122
+"z".ord
+=> 122
 # (What is the significance of the number 122 in relation to the character z?)
+# 122 is the ASCII code for 'z'
 
-# "How many times does the letter 'a' appear in this string?".<???>
-# => 4
+"How many times does the letter 'a' appear in this string? zzzzzzzz".count"a"
+=> 4
