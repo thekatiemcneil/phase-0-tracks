d8c249ada668148d0c1f17810beb2ea2fa186000
thekatiemcneil <katie.mcneil@live.com>
2017-05-16 17:18:15 -0500

    Update with methods.

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,8 +4,8 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
-# => “InVeStIgAtIoN”
+"iNvEsTiGaTiOn".swapcase
+=> “InVeStIgAtIoN”
 
 # "zom".<???>
 # => “zoom”
@@ -32,7 +32,7 @@
 # => "Elementary, my dear Watson!"
 
 # "z".<???>
-# => 122 
+# => 122
 # (What is the significance of the number 122 in relation to the character z?)
 
 # "How many times does the letter 'a' appear in this string?".<???>
