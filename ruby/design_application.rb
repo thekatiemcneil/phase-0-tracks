b76eb52c0b4a858b64dd633a4a695ef5601e229b
thekatiemcneil <katie.mcneil@live.com>
2017-05-16 20:20:04 -0500

    Create and add code for design applicant hash.

---
+++ ruby/design_application.rb
@@ -0,0 +1,21 @@
+applicant_one = {
+  name: "Katie McNeil",
+  address: "123 Main Street Austin, TX 78702",
+  email: "email@gmail.com",
+  phone: "555-555-5555",
+  fave_shade_of_blue: "indigo",
+  wallpaper_preferences: "paisley",
+  ombre_thoughts: "fierce",
+}
+
+p applicant_one
+
+applicant_one[:name] = "Kathryn McNeil"
+applicant_one[:date_of_hire] = "5/25/17"
+
+p applicant_one
+
+p applicant_one[:name] + applicant_one[:address]
+
+p applicant_one
+
