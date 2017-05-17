242b98451d0cbdc247e93d9970294b14abb361fe
thekatiemcneil <katie.mcneil@live.com>
2017-05-17 14:59:09 -0500

    Adding nested data.'

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,50 @@
+concert = {
+  band_1 = {
+    lead_singer: [
+      'Gibson Les Paul',
+      'Pedal Board',
+      'Microphone'
+      ]
+    bassist: [
+      'Fender Jazz',
+      'Pedal Board',
+      'Microphone'
+    ]
+    drummer: [
+      'drum kit',
+      'lots of drum sticks'
+    ]
+    lead_guitarist: [
+      'Fender Telecaster',
+      'Pedal Board',
+      'Microphone'
+    ]
+  }
+  band_2 = {
+    lead_singer: [
+      'Gibson Hummingbird',
+      'Harmonica',
+      'Microphone'
+    ]
+    rhythm_guitarist: [
+      'Gibson Hummingbird',
+      'Microphone'
+    ]
+  }
+  band_3 = {
+    lead_singer: [
+      'Taylor Grand',
+      'Microphone'
+    ]
+    harmony_1: [
+      'Microphone',
+      'Tamborine'
+    ]
+    harmony_2: [
+      'Microphone',
+      'Kick Drum'
+    ]
+  }
+}
+
+p concert
