dddb701749b8d78b2a7ceb6b5e6326041a3de0be
thekatiemcneil <katie.mcneil@live.com>
2017-05-16 20:20:14 -0500

    Add pseudocode.

---
+++ ruby/hashes.rb
@@ -0,0 +1,23 @@
+# create hash with empty keys
+
+# ask for name
+# collect name as variable for name key
+
+# ask for age
+# collect age as variable for age key
+
+# ask for number of children
+# collect number of children as variable for children key
+
+# ask for decor theme
+# collect decor theme as variable for decor key
+
+# ask for number of rooms to be decorated
+# collect number of rooms as variable for rooms key
+
+# print latest version of hash
+
+# ask user if they'd like to update any key, or 'none' to skip
+# if they type a key, assign new variable to that key
+
+# print lastest version of hash
