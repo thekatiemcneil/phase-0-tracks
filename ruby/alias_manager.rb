35f8cfda7946a138e898b0dd0f1f8cc77f4fbe22
thekatiemcneil <katie.mcneil@live.com>
2017-05-19 12:01:08 -0500

    Start from scratch with new algorithm for the challenge.

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,9 @@
+# take in name as a string
+# convert string into a character array
+# test each character to determine if its a vowel
+  # if its a vowel, change to next vowel in sequence
+# test each character to determine if its a consonant
+  # if its a consonant, change to next consonant in sequence
+# return array to string (whats the opposite of chars?)
+# change order of first and last name
+# give output of spy name
