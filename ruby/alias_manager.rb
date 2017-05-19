64d44b4bea1966463e2ae650517cbaed65818935
thekatiemcneil <katie.mcneil@live.com>
2017-05-19 13:01:31 -0500

    Continue to work on methods for vowel conversion.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -7,3 +7,27 @@
 # return array to string (whats the opposite of chars?)
 # change order of first and last name
 # give output of spy name
+
+# vowels = ["a", "e", "i", "o", "u"]
+vowels = (/aeiou/)
+consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "e", "x", "y", "z"]
+
+vowels_hash = {
+  "a" => "e",
+  "e" => "i",
+  "i" => "o",
+  "o" => "u",
+  "u" => "a"
+}
+
+# convert name into character array
+name = "Katie McNeil"
+new_name = name.chars
+p name
+p new_name
+
+# test characters to see if they are vowels
+# name.scan(/[AEIOUaeiou]/)
+# p name.gsub(/["a", "e", "i", "o", "u"]/, '*')
+p name.gsub("aeiou".chars, "eioua".chars)
+p name
