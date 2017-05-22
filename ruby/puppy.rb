8f079179fd47f76bb5a88008a9afa83d7d6567b7
thekatiemcneil <katie.mcneil@live.com>
2017-05-22 15:35:46 -0500

    Add puppy descriptions and behaviors

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+# SPECIES -----------------
+# Puppy
+
+# CHARACTERISTICS ---------
+# Fur color:
+# Fur length:
+# Body size:
+# Name:
+# Bark volume:
+
+# BEHAVIOR ----------------
+# Sniff
+# Run
+# Fetch
+# Eat
