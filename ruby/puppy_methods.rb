cd5cc17f189bd481c59d20dfbce07dd9bebf66d2
thekatiemcneil <katie.mcneil@live.com>
2017-05-22 16:23:12 -0500

    Add methods to puppy.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,32 @@ class Puppy
     toy
   end
 
+  def speak(n)
+    woof_count = n
+    until woof_count == 0
+      puts "Woof!"
+       woof_count -= 1
+    end
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(n)
+    puts n*7
+  end
+
+  def shake
+    puts "*extends paw*"
+  end
+
 end
 
+Bailey = Puppy.new
+
+Bailey.fetch("ball")
+Bailey.speak(3)
+Bailey.roll_over
+Bailey.dog_years(4)
+Bailey.shake
