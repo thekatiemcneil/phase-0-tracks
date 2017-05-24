bdec7963dcf1e328bada72af976b0a4e43890b9a
thekatiemcneil <katie.mcneil@live.com>
2017-05-23 22:38:42 -0500

    Add many santa creation.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,6 @@
 class Santa
+  attr_reader :gender, :ethnicity
+  attr_accessor :age, :reindeer_ranking
 
   def speak
     puts "Ho, ho, ho! Haaaaaapy holidays!"
@@ -10,9 +12,7 @@ class Santa
 
   def initialize(gender, ethnicity)
     puts "Initializing Santa instance...."
-    @gender = gender
-    @ethnicity = ethnicity
-    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    # @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
     @age = 0
   end
 
@@ -31,18 +31,41 @@ class Santa
 
 end
 
+# santas << saint_nick = Santa.new("male", "Kiwi")
+# saint_nick.speak
+# saint_nick.eat_milk_and_cookies("chocolate chip")
+
+# santas << sir_nicholas = Santa.new("gender fluid", "British")
+# santas << nico_las = Santa.new("female", "Kenyan")
+
+# p saint_nick.celebrate_birthday
+# p saint_nick.get_mad_at("Vixen")
+# p saint_nick.celebrate_birthday
+# p sir_nicholas.gender("female")
+
+gender_examples = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+ethnicity_examples = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
 santas = []
 
-santas << saint_nick = Santa.new("male", "Kiwi")
-saint_nick.speak
-saint_nick.eat_milk_and_cookies("chocolate chip")
+# for i in 1..250
+#   santas << Santa.new(gender_examples.sample, ethnicity_examples.sample)
+#   santa.age = (1..140).to_a.sample
+#   puts "We now have #{santas.length}."
+# end
+# santas = []
+# 100.times do |santa_craziness|
+#   gender = gender_examples.sample
+#   ethnicity = ethnicity_examples.sample
+#   age =
+#   santas << Santa.new(gender, ethnicity)
+#   puts "There are now #{santas.length} santas."
+# end
 
-santas << sir_nicholas = Santa.new("gender fluid", "British")
-santas << nico_las = Santa.new("female", "Kenyan")
-
-p saint_nick.celebrate_birthday
-p saint_nick.get_mad_at("Vixen")
-p saint_nick.celebrate_birthday
-p sir_nicholas.gender("female")
-
-p santas
+for i in 1..250
+  gender = gender_examples.sample
+  ethnicity = ethnicity_examples.sample
+  santas = Santa.new(gender, ethnicity)
+  age = (0..140).to_a.sample
+  puts "This santa is a #{gender} #{ethnicity} who is #{age} years old."
+end
