2b7ec11e5d064a7890d47bae18cc5223ed11aa56
thekatiemcneil <katie.mcneil@live.com>
2017-05-25 17:34:29 -0500

    Add initial method for creating a list.

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,41 @@
+
+
+
+# Method to create a list
+def make_a_list(string)
+  grocery_list = {}
+  list_array = string.split(" ")
+  # now we have an array
+  list_array.each do |item|
+    grocery_list[item] = 1
+  end
+end
+
+make_a_list("stuff, things, items")
+
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps:
+  # [fill in any steps here]
+  # set default quantity
+  # print the list to the console [can you use one of your other methods here?]
+# output: [what data type goes here, array or hash?]
+
+# Method to add an item to a list
+# input: list, item name, and optional quantity
+# steps:
+# output:
+
+# Method to remove an item from the list
+# input:
+# steps:
+# output:
+
+# Method to update the quantity of an item
+# input:
+# steps:
+# output:
+
+# Method to print a list and make it look pretty
+# input:
+# steps:
+# output:
