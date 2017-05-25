0e609a17250de76f72873e8acfd743ddae16d683
thekatiemcneil <katie.mcneil@live.com>
2017-05-24 19:12:36 -0500

    udpate standalone module to mixin including test driver code.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,38 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + ":("
+#   end
+
+#   def self.yell_happily(words)
+#     words + "!!!" + "You're the best! :)"
+#   end
+# end
+
+# p Shout.yell_angrily("Hey hello")
+# p Shout.yell_happily("Hey hello")
+
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + ":("
+  def yell_angrily(words)
+    p words + "!!!" + ":( "
   end
 
-  def self.yell_happily(words)
-    words + "!!!" + "You're the best! :)"
+  def yell_happily(words)
+    p words + "!!!" + "You're the best! :)"
   end
 end
 
-p Shout.yell_angrily("Hey hello")
-p Shout.yell_happily("Hey hello")
+class Child
+  include Shout
+end
+
+class Mom
+  include Shout
+end
+
+child = Child.new
+child.yell_angrily("angry yell")
+child.yell_happily("happy yell")
+
+mom = Mom.new
+mom.yell_happily("happy yell")
+mom.yell_angrily("angry yell")
