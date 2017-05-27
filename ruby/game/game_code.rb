3dc50cde72eb1bf71b6a6cd20326b88dc45fa44b
thekatiemcneil <katie.mcneil@live.com>
2017-05-26 22:43:18 -0500

    Add initialize method and test_guess method to WordGame class.

--- ruby/game/game_code.rb
+++ ruby/game/game_code.rb
@@ -1,18 +1,30 @@
 class WordGame
-  attr_accessor :guess_count, :is_over, :guesses_remaining, :correct_guesses
+  attr_accessor :guess_count, :is_over, :guesses_remaining, :current_guess
 
   def initialize(word)
     @allowed_guesses = word.chars.length
-    @guess_count = 0
+    @guess_count = word.length
     @is_over = false
     @word = word
-    @correct_guesses = "-" * word.chars.length.to_i
+    @current_guess = "-" * word.chars.length.to_i
   end
 
   def test_guess(user_guess)
+    user_guess.chars.each do |letter|
+    end
+    for x in @word.chars.length.to_i
+      if
+        @user_guess[x - 1] == @word[x - 1]
+          @current_guess[x - 1] = @user_guess[x - 1]
+      end
+    end
   end
+  # could we use map! to rewrite the array??
+  # What if I used indexes to compare each of the characters of the arrays?
+    # user_guess(x) == word(x)
+      # current_guess(x-1) = user_guess(x)
 
 end
 
 game1 = WordGame.new("word")
-p correct_guesses
+game1.test_guess("cord")
