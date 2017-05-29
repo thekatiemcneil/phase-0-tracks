99c0e26e8e4815b0a28fdb1eff2be1e615c52d00
thekatiemcneil <katie.mcneil@live.com>
2017-05-28 19:59:42 -0500

    Update testing process for methods.

--- ruby/game/game_rspec.rb
+++ ruby/game/game_rspec.rb
@@ -3,8 +3,12 @@ require_relative 'game_code'
 describe WordGame do
   let(:wordgame) { WordGame.new }
 
-  it "" do
+  it "takes in the word to guess" do
     expect(wordgame.word_to_guess("yes")).to eq "yes"
+  end
+
+  it "determines if guess count has been met" do
+    expect(wordgame.too_many_guesses(@guess_count).to eq true
   end
 
 end
