3a0a9c5cd712bf70d1a6b995f69e32e609b94efe
thekatiemcneil <katie.mcneil@live.com>
2017-05-29 12:35:40 -0500

    Attempt to resolve issues with rspec testing.

--- ruby/game/game_rspec.rb
+++ ruby/game/game_rspec.rb
@@ -2,7 +2,6 @@ require_relative 'game_code'
 
 describe WordGame do
   let(:wordgame) { WordGame.new }
-describe :word => "word", :guesses_remaining => 3, :user_guess => "word"
 
   it "takes in the word to guess and defines guess array" do
     expect(wordgame.word_to_guess("yes")).to eq "---"
