07368fdfd52152ff2e3f8ed6e71a81166f4356ea
thekatiemcneil <katie.mcneil@live.com>
2017-05-28 19:14:25 -0500

    Create new methods for game to function.

--- ruby/game/game_code.rb
+++ ruby/game/game_code.rb
@@ -1,10 +1,13 @@
 class WordGame
 attr_accessor :is_over, :guess_count, :word, :user_guess
 
-  def initialize(word)
-    @word = word
+  def initialize
     @is_over = false
     @guess_count = 0
+  end
+
+  def word_to_guess(word)
+    @word = word
   end
 
   def too_many_guesses
@@ -13,6 +16,20 @@ attr_accessor :is_over, :guess_count, :word, :user_guess
     end
   end
 
-
+  def test_guess(user_guess)
+    @user_guess = user_guess
+    if
+      @user_guess == @word
+        @is_over = true
+    else
+      is_over = false
+    end
+    @guess_count += 1
+  end
 
 end
+
+game1 = WordGame.new
+game1.word_to_guess("yes")
+p game1
+game1.test_guess("yes")
