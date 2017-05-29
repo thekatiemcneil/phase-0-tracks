5657470fc4c16feca33166d992e411469ee2bf0a
thekatiemcneil <katie.mcneil@live.com>
2017-05-28 21:14:55 -0500

    Write more tests for other methods.

--- ruby/game/game_rspec.rb
+++ ruby/game/game_rspec.rb
@@ -2,13 +2,27 @@ require_relative 'game_code'
 
 describe WordGame do
   let(:wordgame) { WordGame.new }
+describe :word => "word", :guesses_remaining => 1, :user_guess => "word"
 
-  it "takes in the word to guess" do
-    expect(wordgame.word_to_guess("yes")).to eq "yes"
+  it "takes in the word to guess and defines guess array" do
+    expect(wordgame.word_to_guess("yes")).to eq "---"
   end
 
-  it "determines if guess count has been met" do
-    expect(wordgame.too_many_guesses(@guess_count).to eq true
+# USER_INPUT
+  it "takes in the user guess and converts it to a character array." do
+    expect(wordgame.user_input("mop")).to eq ["m", "o", "p"]
   end
+
+# # TEST_GUESS
+  it "determines if word has been guessed" do
+    expect(wordgame.test_guess).to eq true
+  end
+
+  # TEST_GUESS_COUNT
+  it "checks if guess count has been met" do
+    expect(wordgame.test_guess_count).to eq 0
+  end
+
+# REWRITE_ARRAY
 
 end
