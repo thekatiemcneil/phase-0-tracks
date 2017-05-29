28e2fb196f17cbe9e0f8c22cd9a240a30702a529
thekatiemcneil <katie.mcneil@live.com>
2017-05-28 19:06:11 -0500

    Start from scratch and write initial code.

--- ruby/game/game_code.rb
+++ ruby/game/game_code.rb
@@ -1,30 +1,18 @@
 class WordGame
-  attr_accessor :guess_count, :is_over, :guesses_remaining, :current_guess
+attr_accessor :is_over, :guess_count, :word, :user_guess
 
   def initialize(word)
-    @allowed_guesses = word.chars.length
-    @guess_count = word.length
-    @is_over = false
     @word = word
-    @current_guess = "-" * word.chars.length.to_i
+    @is_over = false
+    @guess_count = 0
   end
 
-  def test_guess(user_guess)
-    user_guess.chars.each do |letter|
-    end
-    for x in @word.chars.length.to_i
-      if
-        @user_guess[x - 1] == @word[x - 1]
-          @current_guess[x - 1] = @user_guess[x - 1]
-      end
+  def too_many_guesses
+    if guess_count >= @word.length
+      @is_over = true
     end
   end
-  # could we use map! to rewrite the array??
-  # What if I used indexes to compare each of the characters of the arrays?
-    # user_guess(x) == word(x)
-      # current_guess(x-1) = user_guess(x)
+
+
 
 end
-
-game1 = WordGame.new("word")
-game1.test_guess("cord")
