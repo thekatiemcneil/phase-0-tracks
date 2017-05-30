aa7bfabca75018ac0031700b17fc46b98b202c40
thekatiemcneil <katie.mcneil@live.com>
2017-05-29 21:58:02 -0500

    Update methods with functionality.

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -1,6 +1,5 @@
 class WordGame
-  attr_reader :number_of_guesses, :guessed_words_array, :is_over, :guessed_letters, :word_to_guess, :current_guess
-  attr_accessor
+  attr_reader :number_of_guesses, :guessed_words_array, :is_over, :guessed_letters, :word_to_guess, :current_guess, :correct_guess
 
   def initialize
     @word = nil
@@ -8,6 +7,7 @@ class WordGame
     @guessed_words_array = []
     @is_over = false
     @guessed_letters = nil
+    @correct_guess = false
   end
 
   def record_word(word)
@@ -23,12 +23,26 @@ class WordGame
   end
 
   def check_guess
+    if @current_guess == @word_to_guess
+      is_over = true
+      correct_guess = true
+    else
+      is_over = false
+      correct_guess = false
+    end
   end
 
   def already_guessed?
+    if @guessed_words_array.include?(@current_guess)
+      @number_of_guesses += 1
+    else
+      @guessed_words_array.push(@current_guess)
+    end
   end
 
   def update_guess_count
+    number_of_guesses -= 1
   end
 
 end
+
