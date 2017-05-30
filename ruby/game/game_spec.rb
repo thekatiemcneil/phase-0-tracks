da3a619f5f3740cbdd11e15f752719c4d504a908
thekatiemcneil <katie.mcneil@live.com>
2017-05-29 22:17:35 -0500

    Continue updating methodology for spec tests.

--- ruby/game/game_spec.rb
+++ ruby/game/game_spec.rb
@@ -18,19 +18,22 @@ describe WordGame do
   end
 
   it "#check_guess checks guess to see if it is correct" do
-    expect(game.check_guess).to eq
+    expect(game.check_guess).to eq true
   end
 
   it "#already_guessed? checks to see if word has already been guessed" do
-    expect(game.already_guessed?).to eq
+    current_guess = "word"
+    word_to_guess = "card"
+    expect(game.already_guessed?).to eq false
   end
 
   it "#update_guess_count updates guess count" do
-    expect(game.update_guess_count).to eq
+    @number_of_guesses = 1
+    expect(game.update_guess_count).to eq 0
   end
 
   it "updates guessed letters string" do
-    expect().to eq
+    expect(game.update_letters_garray).to eq []
   end
 
 end
