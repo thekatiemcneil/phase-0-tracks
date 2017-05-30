2c0214dcd8526b92a6a5dd0668dfaa233ebf0d81
thekatiemcneil <katie.mcneil@live.com>
2017-05-29 20:20:46 -0500

    Define methods needed for game.

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -1,8 +1,9 @@
 class WordGame
-  attr_reader :number_of_guesses, :guessed_words_array, :is_over, :guessed_letters
-  attr_accessor :word_to_guess
+  attr_reader :number_of_guesses, :guessed_words_array, :is_over, :guessed_letters, :word_to_guess, :current_guess
+  attr_accessor
 
   def initialize
+    @word = nil
     @number_of_guesses = 0
     @guessed_words_array = []
     @is_over = false
@@ -11,6 +12,31 @@ class WordGame
 
   def record_word(word)
     @word_to_guess = word
+  end
+
+  def set_guess_count
+    @number_of_guesses = @word_to_guess.length
+  end
+
+  def record_guess(guess)
+    @current_guess = guess
+  end
+
+  def check_guess
+  end
+
+  def already_guessed?
+    if
+      !@guessed_words_array.include?(@current_guess)
+        @guessed_words_array.push(@current_guess)
+    else
+      @guessed_words_array.include?(@current_guess)
+        @number_of_guesses += 1
+    end
+  end
+
+  def update_guess_count
+    @number_of_guesses -= 1
   end
 
 end
