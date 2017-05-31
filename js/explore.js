6346c5186ac1c6a10ea0ce60ebd72bf37e89290a
thekatiemcneil <katie.mcneil@live.com>
2017-05-31 14:51:50 -0500

    Write pesudocode for string reverse, then begin writing function.

---
+++ js/explore.js
@@ -0,0 +1,8 @@
+// Take in a string as an argument (ex. "hello")
+// Create a new, empty string
+// Iterate through each charater in original string and assign a new index value (ex. originalstring(0) would be come newstring(-1), originalstring(1) would become newstring(-2).)
+// after iteration is complete, print the resulting new string
+
+var str = "hello"
+
+for (var i = 0; i <  )
