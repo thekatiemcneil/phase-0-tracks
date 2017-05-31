078a9dc65af9550d77544a8ca4760e04c48b851f
thekatiemcneil <katie.mcneil@live.com>
2017-05-31 17:31:07 -0500

    Write, test, and re-write function to reverse strings.

--- js/explore.js
+++ js/explore.js
@@ -3,6 +3,12 @@
 // Iterate through each charater in original string and assign a new index value (ex. originalstring(0) would be come newstring(-1), originalstring(1) would become newstring(-2).)
 // after iteration is complete, print the resulting new string
 
-var str = "hello"
+function reverseStr(string) {
+  var newstr = '';
+  for (var i = string.length - 1; i >= 0; i--)
+    newstr += string[i];
+  return newstr;
+}
 
-for (var i = 0; i <  )
+console.log(reverseStr("hello"))
+console.log(reverseStr("DevBootcamp"))
