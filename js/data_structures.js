c536f78e567fff88fb149aa7b3ec0bd25a274e77
thekatiemcneil <katie.mcneil@live.com>
2017-06-03 11:53:53 -0500

    Create objects with function as property and create a few cars.

--- js/data_structures.js
+++ js/data_structures.js
@@ -18,3 +18,25 @@ function createHorses(names, colors)
 }
 var horses = createHorses(names, colors)
 console.log(horses)
+
+function Car(make, color, year) {
+  this.make = make;
+  this.color = color;
+  this.year = year;
+
+  this.vroom = function() {console.log("Vroom vroom!"); };
+}
+
+console.log("Lets build a car!")
+
+var anotherCar = new Car("Jeep", "Red", 1993);
+console.log(anotherCar);
+console.log("Our car is speedy!");
+anotherCar.vroom();
+console.log("--------");
+console.log("Lets build another car!");
+var yetAnotherCar = new Car("Buddy", "Lime Green", 2016);
+console.log(yetAnotherCar);
+console.log("This car is actually a scooter, but it can still go fast!");
+yetAnotherCar.vroom();
+console.log("-------");
