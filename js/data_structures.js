1c73ea0b031c2f74ac8082140c396ef248bfefc2
thekatiemcneil <katie.mcneil@live.com>
2017-06-03 11:37:16 -0500

    Create method to merge colors and names into an object.

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,2 +1,20 @@
 var colors = ["cerulean", "sparkle", "rainbow", "glitter"]
 var names = ["Leslie", "Ron Swanson", "Andy", "Jean-Ralphio"]
+
+colors.push("surprise color")
+names.push("Lil Sebastian")
+
+console.log(colors)
+console.log(names)
+
+function createHorses(names, colors)
+{
+  var horses = {}
+  if (names.length != colors.length)
+    return null;
+  for(var index in names)
+    horses[names[index]] = colors[index];
+  return horses;
+}
+var horses = createHorses(names, colors)
+console.log(horses)
