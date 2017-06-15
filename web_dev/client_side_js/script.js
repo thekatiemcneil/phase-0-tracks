78880e49cb8da5b17b2f99426b6c1c485eef17e3
thekatiemcneil <katie.mcneil@live.com>
2017-06-14 20:53:30 -0500

    Update script to manipulate characteristics of various classes and IDs using JS.

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,10 +1,24 @@
 console.log("The script is running!");
 
-function addPinkBorder(event) {
+function addRedBorder(event) {
   console.log("click happened! here's the click event:");
   console.log(event);
-  event.target.style.border = "2px solid pink";
+  event.target.style.border = "2px solid red";
+}
+
+function changeColor(event) {
+  event.target.style.color = "blue";
+}
+
+function changeFontColor(event) {
+  event.target.style.color = "orange";
 }
 
 var photo = document.getElementById("lizard-photo");
-photo.addEventListener("click", addPinkBorder);
+photo.addEventListener("click", addRedBorder);
+
+var thing1 = document.getElementById("thing1");
+thing1.addEventListener("click", changeColor);
+
+var title = document.getElementById("title");
+title.addEventListener("click", changeFontColor);
